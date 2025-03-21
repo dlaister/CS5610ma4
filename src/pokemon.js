88c05f1758e30ec789e60a680c81a06f2f9bde36
dlaister <derek_laister@outlook.com>
2025-03-20 22:41:51 -0700

    adding delete api

--- src/pokemon.js
+++ src/pokemon.js
@@ -9,14 +9,19 @@ console.log(`My name is ${name}`)
 
 // use this list as your temporary database!
 // note that it will reset every time you restart your server
+// updated example to look like class example, more readable
 const myPokemon = [{
-    id: "fc10b559-872c-43cd-bad2-f02e2e0a2d58", name: "Pikachu", health: 10, level: 1
+    id: "fc10b559-872c-43cd-bad2-f02e2e0a2d58",
+    name: "Pikachu",
+    health: 10,
+    level: 1
 }];
 
 router.get('/', function (req, res) {
     // return all pokemon
 });
 
+// Add entery to the list
 router.post('/', (req, res) => {
     // if the pokemon name already exists in the list, return an error
     // randomly generate an id using UUID ["uuid()"]
@@ -26,66 +31,77 @@ router.post('/', (req, res) => {
     // return a 200
 });
 
+// Fetch entry from the list
 router.get('/:pokemonId', function (req, res) {
     // return pokemon if one is found matching the pokemonId
     // return a 404 if no pokemon matches that pokemonId
 });
 
+// Update entry from the list
 router.put('/:pokemonId', function (req, res) {
     // update the pokemon matching the pokemonId
     // based on the req body
     // return a 404 if no pokemon matches that pokemonId  
 })
 
+// Delete entry from the list
 router.delete('/:pokemonId', function (req, res) {
     // delete pokemon if pokemonId matches the id of one
     // return 200 even if no pokemon matches that Id
-})
+    const pokemonId = req.params.pokemonId;
+    const index = myPokemon.findIndex(function (pokemon) {
+        return pokemon.id === pokemonId;
+    });
+
+    if (index !== -1) {
+        console.log(`Deleting Pokemon with ID: ${pokemonId}`);
+        myPokemon.splice(index, 1);
+    } else {
+        console.log(`No Pokemon found with ID: ${pokemonId}`);
+    }
+
+    res.status(200).json({ message: "Pokemon deleted (if existed)" });
+});
 
 module.exports = router;
 
 
 
-
-
-
-
+// MY NOTES, THESE CAN BE IGNORED.
 
 // router.{request here}(/url here, function that you define here)
-
-
-//id
-router.get('/:pokemonId', function (request, response) {
-    const Id = request.params.pokemonId;
-
-    const responsePokemon = myPokemon[Id];
-
-    // if (!responsePokemon = myPokemon[Id]) {
-    //     responsePokemon.status = 404;
-    //     response.send('Not Found');
-    //     return;
-    // }
-
-    response.json(responsePokemon);
-
-})
-
-// querey param example
-router.post('/', function (request, response) {
-    const pokemonName = request.query.name;
-
-    if (pokemonName) {
-        const matchingPokemon = []
-        const pokemonList = Object.values(myPokemon)
-
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            if (pokemonName === pokemon.name) {
-                matchingPokemon.push(pokemon);
-            }
-        }
-    }
-})
-
 // install nodemon (https://alecktos.medium.com/intellij-debugging-with-nodemon-4ebd121b18f1):
 // npx nodemon --inspect app.js
+
+//id
+// router.get('/:pokemonId', function (request, response) {
+//     const Id = request.params.pokemonId;
+//
+//     const responsePokemon = myPokemon[Id];
+//
+//     // if (!responsePokemon = myPokemon[Id]) {
+//     //     responsePokemon.status = 404;
+//     //     response.send('Not Found');
+//     //     return;
+//     // }
+//
+//     response.json(responsePokemon);
+//
+// })
+
+// query param example
+// router.post('/', function (request, response) {
+//     const pokemonName = request.query.name;
+//
+//     if (pokemonName) {
+//         const matchingPokemon = []
+//         const pokemonList = Object.values(myPokemon)
+//
+//         for (let i = 0; i < pokemonList.length; i++) {
+//             const pokemon = pokemonList[i];
+//             if (pokemonName === pokemon.name) {
+//                 matchingPokemon.push(pokemon);
+//             }
+//         }
+//     }
+// })
